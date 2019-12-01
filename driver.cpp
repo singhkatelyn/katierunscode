b6a7afa85804fbd1be5d5500bb4c8b29077e7587
singhkatelyn <57650834+singhkatelyn@users.noreply.github.com>
2019-11-30 17:08:42 -0700

    Update driver.cpp

--- driver.cpp
+++ driver.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <vector>
+#include <sstream>
 #include "Graph.hpp"
 #include "LinkedList.hpp"
 using namespace std;
 
-int main()
+int main(int argc, char *argv[]) //1: .csv
 {
   string EWStCurr;
   string NSStCurr;
@@ -19,12 +20,32 @@ int main()
   Graph g;
   g.loadVertex();
   g.loadEdges();
-    //read in from file
-    //while(getline())
-    //{
+  
+  //declare string to hold the stores from file
+  string word; 
+  
+  //declare things to be passed into insertStore
+  store* prev;
+  string storeName;
+  
+  ifstream fin(argv[1]); //assign the csv file to fin
+  //read in from file
+  if(fin.is_open())
+  {
+    while(getline(fin, stores))//read in line by line
+    {
+      sringstream ss(stores); //use string stream to separate words
+      while(ss >> word) //what am I doing here
+      {
+        if(word != "NULL")//fill linked list with words until NULL??
+        {
+          
+        }
+      }
       //fill LL at each vertex of the graph (intersection)
-    //}
-
+      l.insertStore(prev, storeName);
+    }
+  }
 
     cout << "Enter the street you are on that runs North to South. Use the form 'StreetNameAve' e.i. '8thAve' or 'FashionAve'." << endl;
     cin >> NSStCurr;
