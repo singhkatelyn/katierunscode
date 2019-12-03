e1fc70cbe15f052bd246a1215c261c303b04b902
SeokSong <55889217+SeokSong@users.noreply.github.com>
2019-12-02 20:17:03 -0700

    Update Graph.hpp

--- Graph.hpp
+++ Graph.hpp
@@ -5,23 +5,24 @@
 #include <iostream>
 using namespace std;
 
-struct vectex;
+struct vertex;
 struct adjVertex{
-  vertex *v;
-  int weight;
+    vertex *v;
+    int weight;
 };
 
 struct vertex{
-  vertex(){
-    this->visited = false;
-    this->distance = 0;
-    this->previous = NULL;
-  }
-  string name;
-  bool visited;
-  int distance;
-  vertex* previous;
-  vector<adjVertex> adj;
+    vertex() {
+        this->visited = false;
+        this->color = "";
+        this->distance = 0;
+    }
+    string name;
+    bool visited;
+    string color;
+    int distance;
+    vertex *pred;
+    vector<adjVertex> adj;
 };
 
 class NewYork{
@@ -30,11 +31,14 @@ class NewYork{
   public:
     void addEdge(string v1, string v2, int weight);
     void addVertex(string name);
-    void displayEdges();
-    void depthFirstTraversal(string sourceVertex);
+    void loadVertex();
+    void loadEdges();
+    void displayEdges(); //use this only for testing
+    // void breadthFirstTraverse(string sourceVertex, string);
+    vertex* searchGraph(string);
     vertex* DijkstraAlgorithm(string start, string end);
-    void directions(string start, string end);
-    void setAllVerticesUnvisited();
+    void findStoreType(string, string);
+    void printDirections(string start, string end);
 };
 
 #endif
